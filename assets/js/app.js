c9157f1fc4303957af17948393c9f1471c0f0df5
Hanawa Hinata <bainesing@nebula-soft.com>
2020-01-29 19:21:34 +0800

    update weather function

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,24 @@
  * 运作脚本使用标准原生 JavaScript ES6 编写方式，基于 Vue 开发
  * 版权所有 2020 (c) http://tyiet.cn AllRight Reserved.
  */
+console.log(
+    `%c nonghi-dp project written for %c Vue %c`,
+    'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+    'background:#41b883 ; padding: 1px; border-radius: 0 3px 3px 0;  color: #fff',
+    'background:transparent'
+);
+console.log("Copyright (c) 2020 http://tyiet.cn AllRight Reserved. Written by Hanawa Hinata on 2020/1/25");
+
+
+
 
 var  app = new Vue({
     el: '#app',
     data: {
         pageIsLoading:true,
 
-        currentTime: '2020-01-25 18:46:30',
+        currentTime: '8888-88-88 88:88:88',
+        timeInterval:null,
 
         weatherInfo:{
             basic:{},
@@ -29,13 +40,32 @@ var  app = new Vue({
                 })
             });
         },
+
+        updateCurrentTime:function (mode) {
+            return new Promise((result) => {
+                let time = (mode === 'now'?new Date().getTime():'');
+                let DataObject = new Date(time);
+                //手动格式化时间
+                let timeString = DataObject.getFullYear()+"-"+
+                    ((DataObject.getMonth()+1)<10?'0'+(DataObject.getMonth()+1):(DataObject.getMonth()+1))+"-"+
+                    (DataObject.getDate()<10?'0'+DataObject.getDate():DataObject.getDate()) + ' ' +
+                    (DataObject.getHours()<10?'0'+DataObject.getHours():DataObject.getHours()) + ':' +
+                    (DataObject.getMinutes()<10?'0'+DataObject.getMinutes():DataObject.getMinutes()) + ':' +
+                    (DataObject.getSeconds()<10?'0'+DataObject.getSeconds():DataObject.getSeconds());
+                result(timeString);
+            })
+        }
     },
     created: function (options) {
         // do nothing...
     },
     mounted: async function () {
+        let _this = this;
         //获取天气
         this.weatherInfo = await this.getWeatherInfo();
+        this.timeInterval = setInterval(async function () {
+            _this.currentTime = await _this.updateCurrentTime('now');
+        },1000);
 
         //关闭加载状态
         this.pageIsLoading = false;
